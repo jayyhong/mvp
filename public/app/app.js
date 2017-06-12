488c4b68371ffca40084bf30b6191d0390804135
Jay Hongvarivatana <jayyhong@gmail.com>
2017-06-12 13:24:09 -0700

    update

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,13 @@
 angular.module('app')
 
 .controller('appCtrl', function() {
+  var count = 0;
   this.message = "Flexing on that finger"
 
+  this.spaceBar = function() {
+    count++
+    console.log(count)
+  }
 })
 
 .directive('app', function() {
